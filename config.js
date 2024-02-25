730946022052859c40de359692e107215d9ffc58
luisonanaguilera <161082207+luisonanaguilera@users.noreply.github.com>
2024-02-25 01:34:29 -0600

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
-global.confirmCode = ""
+global.botnumber = "50492138147"
+global.confirmCode = "50492138147"
 
 global.owner = [
   ['5219992095479', '👑 Mystic - Creador 👑', true],
